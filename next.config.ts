74c7a087e98b44bef03160674d267012cc9eb17c
Ceft <146732550+Ceft7412@users.noreply.github.com>
2026-02-09 13:59:28 +0800

    Fix the external module workings.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["pdfjs-dist"],
   experimental: {
     serverActions: {
       bodySizeLimit: "50mb",
